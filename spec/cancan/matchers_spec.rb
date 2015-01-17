0acb7c86c6f7e81f8ac929723f74482a0db37c57
Theeban Soundararajan <theeban@intersect.org.au>
2015-01-16 19:29:41 -0800

    refactored matchers spec

--- spec/cancan/matchers_spec.rb
+++ spec/cancan/matchers_spec.rb
@@ -1,33 +1,42 @@
 require "spec_helper"
 
-describe "be_able_to" do
-  it "delegates to can?" do
-    object = Object.new
-    object.should_receive(:can?).with(:read, 123) { true }
-    object.should be_able_to(:read, 123)
+describe '#be_able_to' do
+  subject(:object) { spy('object', can?: can_value) }
+
+  let(:ability) { :read }
+  let(:first_arg) { 123 }
+  let(:args) { [first_arg] }
+  let(:can_value) { true }
+
+  describe 'delegates to can?' do
+
+    before do
+      expect(object).to be_able_to(ability, *args)
+    end
+
+    context 'when called with single arg' do
+      it { expect(object).to have_received(:can?).with(ability, *args) }
+    end
+
+    context 'when called with multiple args' do
+      let(:second_arg) { 456 }
+      let(:args) { [first_arg, second_arg] }
+      it { expect(object).to have_received(:can?).with(ability, *args) }
+    end
+
   end
 
-  it "reports a nice failure message for should" do
-    object = Object.new
-    object.should_receive(:can?).with(:read, 123) { false }
-    expect do
-      object.should be_able_to(:read, 123)
-    end.should raise_error('expected to be able to :read 123')
+  describe 'reports a nice failure message' do
+
+    context 'when expected to be able to on object that cannot' do
+      let(:can_value) { false }
+      it { expect{ expect(object).to be_able_to(ability, *args) }.to raise_error("expected to be able to :#{ability} #{first_arg}") }
+    end
+
+    context 'when not expected to be able to object that can' do
+      it { expect{ expect(object).not_to be_able_to(ability, *args) }.to raise_error("expected not to be able to :#{ability} #{first_arg}") }
+    end
+
   end
 
-  it "reports a nice failure message for should not" do
-    object = Object.new
-    object.should_receive(:can?).with(:read, 123) { true }
-    expect do
-      object.should_not be_able_to(:read, 123)
-    end.should raise_error('expected not to be able to :read 123')
-  end
-
-  it "delegates additional arguments to can? and reports in failure message" do
-    object = Object.new
-    object.should_receive(:can?).with(:read, 123, 456) { false }
-    expect do
-      object.should be_able_to(:read, 123, 456)
-    end.should raise_error('expected to be able to :read 123 456')
-  end
 end
